a638cf1024566dc933a1734a13513509a65107e7
Alex Bannon <alex.bannon@mobileposse.com>
2017-04-28 14:31:22 -0400

    added existence tests and stubbing methods in script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,49 @@
 (function (window) {
 
   function Logger() {
-    
+    this.level = {
+      DEBUG: false,
+      INFO: false,
+      WARN: false,
+      ERROR: false,
+      OFF: true
+    }
+    this.debug = function() {
+
+    }
+    this.info = function() {
+
+    }
+    this.warn = function() {
+
+    }
+    this.error = function() {
+
+    }
+    this.getLevel = function() {
+      for (var key in this.level) {
+        if (this.level.hasOwnProperty(key)) {
+          if (this.level[key] === true) {
+            return key
+          }
+        }
+      }
+    }
+    this.setLevel = function(level) {
+      logLevel = level.toUpperCase();
+      if (this.level[logLevel] !== undefined) {
+        for (var key in this.level) {
+          if (this.level.hasOwnProperty(key)) {
+            if (key == logLevel) {
+              this.level[key] = true
+            } else {
+              this.level[key] = false
+            }
+          }
+        }
+
+      }
+    }
   }
 
   window.logger = new Logger();
--- test/logger_test.js
+++ test/logger_test.js
@@ -1,39 +1,50 @@
 'use strict';
 var expect = chai.expect;
 var should = chai.should();
-
-/* Necessary Functionality:
+/*
+Done
 The logger should be exposed on the window object as window.logger
 The logger should support getting and setting a log level via (logger.getLevel and logger.setLevel respectively).
+The logger should default to the logger.level.OFF level.
 The logger should have methods debug, info, warn, and error.
 The log levels should be, correspondingly, logger.level.DEBUG, logger.level.INFO, logger.level.WARN, logger.level.ERROR, and logger.level.OFF.
+
+*/
+/* Necessary Functionality:
 If the logger is set to level logger.level.WARN and the info method is called, then the logger should do nothing.
 However, if the warn method is called, the logger should call console.warn with the passed in arguments.
 
 If error method is called, the logger should call console.error with the passed in arguments.
 If set to the logger.level.OFF level, the logger should not log anything
 
-The logger should default to the logger.level.OFF level.
 
 The logger should have a context method, which accepts a string as an argument.
 This method should return an object with methods debug, info, warn, and error
 */
 
 describe('Logger', function() {
-  it('should be available on the window object', function(done) {
+  it('should be available on the window object', function() {
     expect(window.logger).to.not.be.undefined
-    done()
   });
-  it('should support getting a log level', function(done) {
-    expect(window.logger.getLevel()).to.not.be.undefined
-    done()
-  });
-  it('should default to an off log level', function(done) {
-    expect(window.logger.getLevel()).should.equal('OFF')
+  it('should not expose the object constructor to the window', function() {
+    expect(window.Logger).to.be.undefined
   })
-  it('should support setting a log level', function(done) {
+  it('should support getting a log level', function() {
+    expect(window.logger.getLevel()).to.not.be.undefined
+  });
+  it('should default to an off log level', function() {
+    expect(window.logger.getLevel()).to.equal('OFF')
+    expect(window.logger.level.OFF).to.equal(true)
+  })
+  it('should support setting a log level', function() {
     var currentLevel = window.logger.getLevel();
     window.logger.setLevel('info');
-    expect(window.logger.getLevel()).should.equal('INFO')
+    expect(window.logger.getLevel()).to.equal('INFO')
+  })
+  it('should support main method functionality', function() {
+    expect(window.logger.debug).to.be.a('function')
+    expect(window.logger.info).to.be.a('function')
+    expect(window.logger.warn).to.be.a('function')
+    expect(window.logger.error).to.be.a('function')
   })
 })
