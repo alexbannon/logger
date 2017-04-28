274468c34317d2115836c412c8187b29c388c36a
Alex Bannon <alex.bannon@mobileposse.com>
2017-04-28 13:22:44 -0400

    initial setup of server and mocha complete

--- server/server.js
+++ server/server.js
@@ -4,20 +4,9 @@ var path = require('path');
 var port     = process.env.PORT || 3000;
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var expressValidator = require('express-validator'); // validating the POST request in query controller
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var root = process.env.root || path.normalize(__dirname + '/../');
 
-var redis = require('redis');
-var redisClient = redis.createClient();
-
-redisClient.on('ready',function() {
-  console.log('Redis is ready');
-});
-
-redisClient.on('error',function(err) {
-  console.log('Error in Redis: ' + err);
-});
 // configuration ===============================================================
 app.use(express.static(root + '/public/'));
 app.use(morgan('dev'));                                         // log every request to the console
@@ -34,5 +23,5 @@ app.get('/', function(req, res) {
 
 
 // listen (start app with node server.js) ======================================
-app.listen(port, '0.0.0.0');
+app.listen(port);
 console.log('App listening on port ' + port);
---
+++ test/logger_test.js
@@ -0,0 +1,10 @@
+'use strict';
+// Fake Test for Setup
+
+describe('addition', function () {
+  it('should add 1+1 correctly', function (done) {
+    var onePlusOne = 1 + 1;
+    onePlusOne.should.equal(2);
+    done();
+  });
+});
